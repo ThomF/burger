7cef3dad59351f443c2e27bdf955ae03c56726c5
ThomF <seniorswanky@icloud.com>
2023-02-13 15:53:08 -0700

    c

---
+++ server/db/FakeDb.js
@@ -0,0 +1,17 @@
+class FakeDb {
+
+    Burgers = [
+        {
+            id: '1',
+            name: 'cheese burger'
+        }, {
+            id: '2',
+            name: 'OG burger'
+        }
+
+
+    ]
+
+}
+
+export const fakeDb = new FakeDb()
---
+++ server/services/BurgersServices.js
@@ -0,0 +1,9 @@
+import { fakeDb } from "../db/FakeDb.js"
+import { BadRequest } from "../utils/Errors.js"
+
+
+class BurgersService {
+
+}
+
+export const burgersService = new BurgersService()
